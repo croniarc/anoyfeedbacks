146ff1047c09a92c54d23f0145bf093e09cf2786
croniarc <croniarc@gmail.com>
2024-06-10 00:19:22 +0530

    db connection added

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection : ConnectionObject = {}
+
+async function dbConnect():Promise<void> {
+    if (connection.isConnected){
+        console.log("Already Connected to database")
+        return
+    }
+
+    try{
+        const db = await mongoose.connect(process.env.MONGODB_UR || "", {})
+
+        connection.isConnected = db.connections[0].readyState
+        console.log("DB Connected Successfully")
+    }catch(err){
+        console.log("DB Connection Failed", err)
+        process.exit(1)
+    }
+}
+
+export default dbConnect;
