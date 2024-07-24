0c6547a61067a694e8e8cf1311ede335bc342f3e
croniarc <croniarc@gmail.com>
2024-07-24 08:40:14 +0530

    some canegs layout, page and authProvider

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,20 @@
+"use client"
 import React from 'react'
-
-const signIn = () => {
+import { useSession, signIn, signOut } from "next-auth/react"
+export default function Component() {
+  const {data: session} = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button className="bg-orange-500 px=3 py-1 m-4 rounded" onClick={() => signOut()}> Sign Out</button>
+      </>
+    )
+  }
   return (
-    <div>
-      signIn
-    </div>
+    <>
+      Not signed in <br />
+      <button className="bg-orange-500 px=3 py-1 m-4 rounded" onClick={() => signIn()}> Sign In</button>
+    </>
   )
 }
-
-export default signIn
